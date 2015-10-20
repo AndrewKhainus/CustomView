3b93818d773963ecf15fde9d1c22cc21ba3f5c63
Andrew Khaynus <a.khaynus@gmail.com>
2015-10-20 16:48:21 +0300

    new customView

--- app/src/main/java/com/radomar/customview/MainActivity.java
+++ app/src/main/java/com/radomar/customview/MainActivity.java
@@ -3,7 +3,6 @@ package com.radomar.customview;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.radomar.customview.circleMenu.TestViewGroup;
 
 public class MainActivity extends Activity {
 
@@ -11,18 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final TestViewGroup testViewGroup = (TestViewGroup)findViewById(R.id.testViewGroup);
-
-        int[] menuItems = {R.drawable.abc_ic_clear_mtrl_alpha,
-                R.drawable.abc_ic_voice_search_api_mtrl_alpha, R.drawable.abc_ic_menu_moreoverflow_mtrl_alpha,
-                R.drawable.abc_ic_menu_copy_mtrl_am_alpha, R.drawable.abc_ic_menu_selectall_mtrl_alpha,
-                R.drawable.abc_ic_menu_paste_mtrl_am_alpha, R.drawable.abc_ic_menu_copy_mtrl_am_alpha, };
-
-//        int[] menuItems = {R.drawable.abc_ic_voice_search_api_mtrl_alpha, R.drawable.abc_ic_clear_mtrl_alpha,
-//                R.drawable.abc_ic_menu_paste_mtrl_am_alpha, R.drawable.abc_ic_menu_copy_mtrl_am_alpha,};
-        testViewGroup.initMenu(menuItems);
-
 
     }
 
